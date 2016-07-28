b3aabf44143af2b18851be7cc28268c0019f001c
Natalielim <natalielim@Natalies-MacBook-Pro.local>
2016-07-28 10:33:51 -0700

    App for mom's birthday

---
+++ M A M A/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  M A M A
+//
+//  Created by Natalie Lim on 7/27/16.
+//  Copyright © 2016 Natalie Lim. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
